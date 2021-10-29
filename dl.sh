591f8ad714b1316b94ca1c167f6ca58bcde0a127
Dirk Van Haerenborgh <dirk.vanhaerenborgh@aloxy.io>
2021-10-29 06:46:35 +0200

    add javascriptcore

--- dl.sh
+++ dl.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 set -e
 
-./gir-dl.sh https://packages.debian.org/stable/amd64/libatk1.0-dev/download http.us.debian.org
-./gir-dl.sh https://packages.debian.org/stable/amd64/libgirepository1.0-dev/download http.us.debian.org
-./gir-dl.sh https://packages.debian.org/stable/amd64/libgraphene-1.0-dev/download http.us.debian.org
-./gir-dl.sh https://packages.debian.org/stable/amd64/libgdk-pixbuf-2.0-dev/download http.us.debian.org
-./gir-dl.sh https://packages.debian.org/stable/amd64/libgtk-3-dev/download http.us.debian.org
-./gir-dl.sh https://packages.debian.org/stable/amd64/libgtk-4-dev/download http.us.debian.org
-./gir-dl.sh https://packages.debian.org/stable/amd64/libharfbuzz-dev/download http.us.debian.org
-./gir-dl.sh https://packages.debian.org/stable/amd64/libpango1.0-dev/download http.us.debian.org
-./gir-dl.sh https://packages.debian.org/stable/amd64/libjavascriptcoregtk-4.0-dev/download http.us.debian.org
-./gir-dl.sh https://packages.debian.org/stable/amd64/libsoup2.4-dev/download http.us.debian.org
-./gir-dl.sh https://packages.debian.org/stable/amd64/libwebkit2gtk-4.0-dev/download http.us.debian.org
+./gir-dl.sh https://packages.debian.org/unstable/amd64/libatk1.0-dev/download http.us.debian.org
+./gir-dl.sh https://packages.debian.org/unstable/amd64/libgirepository1.0-dev/download http.us.debian.org
+./gir-dl.sh https://packages.debian.org/unstable/amd64/libgraphene-1.0-dev/download http.us.debian.org
+./gir-dl.sh https://packages.debian.org/unstable/amd64/libgdk-pixbuf-2.0-dev/download http.us.debian.org
+./gir-dl.sh https://packages.debian.org/unstable/amd64/libgtk-3-dev/download http.us.debian.org
+./gir-dl.sh https://packages.debian.org/unstable/amd64/libgtk-4-dev/download http.us.debian.org
+./gir-dl.sh https://packages.debian.org/unstable/amd64/libharfbuzz-dev/download http.us.debian.org
+./gir-dl.sh https://packages.debian.org/unstable/amd64/libpango1.0-dev/download http.us.debian.org
+
+./gir-dl.sh https://packages.debian.org/unstable/amd64/libjavascriptcoregtk-4.0-dev/download http.us.debian.org
+./gir-dl.sh https://packages.debian.org/unstable/amd64/libsoup2.4-dev/download http.us.debian.org
+./gir-dl.sh https://packages.debian.org/unstable/amd64/libwebkit2gtk-4.0-dev/download http.us.debian.org
 
 ./reformat.sh
 ./fix.sh
--- fix.sh
+++ fix.sh
@@ -141,3 +141,23 @@ xmlstarlet ed -L \
 	-u '//_:constant[@name="INVALID_LIST_POSITION"]/_:type/@name' -v "guint" \
 	-u '//_:constant[@name="INVALID_LIST_POSITION"]/_:type/@c:type' -v "guint" \
 	Gtk-4.0.gir
+
+# change int to uint to prevent overflow
+xmlstarlet ed -L \
+	-u '//_:constant[@name="DOM_NODE_FILTER_SHOW_ALL"]/_:type[@name="gint"]/@c:type' -v "guint" \
+	-u '//_:constant[@name="DOM_NODE_FILTER_SHOW_ALL"]/_:type[@name="gint"]/@name' -v "guint" \
+	WebKit2WebExtension-4.0.gir
+
+xmlstarlet tr JavaScriptCore-4.0.xsl JavaScriptCore-4.0.gir | xmlstarlet fo >JavaScriptCore-4.0.gir.tmp
+mv JavaScriptCore-4.0.gir.tmp JavaScriptCore-4.0.gir
+
+# fill in types from JavaScriptCore
+xmlstarlet ed -L \
+	-i '///_:type[not(@name) and @c:type="JSGlobalContextRef"]' -t 'attr' -n 'name' -v "JavaScriptCore.GlobalContextRef" \
+	-i '///_:type[not(@name) and @c:type="JSValueRef"]' -t 'attr' -n 'name' -v "JavaScriptCore.ValueRef" \
+	WebKit2WebExtension-4.0.gir WebKit2-4.0.gir
+
+xmlstarlet ed -L \
+	-u '//_:constant[@name="DOM_NODE_FILTER_SHOW_ALL"]/_:type/@name' -v "guint" \
+	-u '//_:constant[@name="DOM_NODE_FILTER_SHOW_ALL"]/_:type/@c:type' -v "guint" \
+	WebKit2WebExtension-4.0.gir
